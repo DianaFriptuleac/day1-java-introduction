d072391eced1b43a76006a01bd1893e0e09d61d8
Diana <dianadorojuc3@gmail.com>
2024-09-23 13:25:49 +0200

    Moltiplicazione e concatenazione

---
+++ src/FirstMain.java
@@ -0,0 +1,12 @@
+public class FirstMain {
+    //Moltiplicazione di due nr. interi
+    public static int moltiplicazione(int x, int y) {
+        return x * y;
+    }
+//Concatena una stringa e un numero
+    public static String concatena (String stringa, int numero){
+        return stringa + numero;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+//Moltiplicazione
+        int nr1 = 10;
+        int nr2 = 2;
+        System.out.println("Il risultato della moltiplicazione è " + FirstMain.moltiplicazione(nr1, nr2));
+
+        //Concatenare la stringa e il nr intero
+        String mystring = "Il numero intero è ";
+        int mynumber = 10;
+
+        System.out.println("Concatena stringa e numero: " + FirstMain.concatena(mystring, mynumber));
+
+    }
+}
