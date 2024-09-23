db42640387c3c4d8404c0154016121fe6b6e77d4
Diana <dianadorojuc3@gmail.com>
2024-09-23 19:01:30 +0200

    versione piu semplice del metodo per inserire una stringa nel array

--- src/FirstMain.java
+++ src/FirstMain.java
@@ -3,26 +3,30 @@ public class FirstMain {
     public static int moltiplicazione(int x, int y) {
         return x * y;
     }
-//Concatena una stringa e un numero
+   //Concatena una stringa e un numero
     public static String concatena (String stringa, int numero){
         return stringa + numero;
     }
 
     // Metodo per inserire una stringa in un array di 5 elementi e restituire un nuovo array di 6 elementi
-    public static String[] insertString(String[] array, String newString) {
+    public static String[] inserisciInArray(String[] array, String newString) {
         String[] newArray = new String[6]; // Nuovo array con 6 elementi
 
-        // Copio i primi 2 elementi nel nuovo array
-        for (int i = 0; i < 2; i++) {
-            newArray[i] = array[i];
-        }
+        for (int i = 0; i <newArray.length; i++){
+            //Se i è inferiore a 2, copio gli elementi originali nelle prime due posizioni del nuovo array
+            if (i < 2) {
+                //Copio i primi due elementi dal vecchio array al nuovo
 
-        // Inserisco la nuova stringa nella terza posizione (indice 2)
-        newArray[2] = newString;
-
-        // Copio i restanti elementi dell'array originale
-        for (int i = 3; i < newArray.length; i++) {
-            newArray[i] = array[i - 1];
+                newArray[i] = newString;
+            } else if (i ==2){
+                //Se i è uguale a 2, inserisco la nuova stringa alla terza posizione
+                //inserisco la nuova stringa alla posizione 2
+                newArray[i] = newString;
+            } else {
+                //Altrimenti, copio i restanti elementi dal vecchio array, partendo da i - 1, nel nuovo array
+                //copia i restanti elementi dal vecchio array al nuovo array
+                newArray[i] = array[i - 1];
+            }
         }
 
         return newArray;
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-//Moltiplicazione
+        //Moltiplicazione
         int nr1 = 10;
         int nr2 = 2;
         System.out.println("Il risultato della moltiplicazione è " + FirstMain.moltiplicazione(nr1, nr2));
@@ -8,7 +8,6 @@ public class Main {
         //Concatenare la stringa e il nr intero
         String mystring = "Il numero intero è ";
         int mynumber = 10;
-
         System.out.println("Concatena stringa e numero: " + FirstMain.concatena(mystring, mynumber));
 
         //Una nuova stringa in un array e restituire un array di 6 elementi
@@ -18,7 +17,7 @@ public class Main {
         String newString = "Padova";
 
         // Chiamo il metodo insertString per ottenere il nuovo array con 6 elementi dentro
-        String[] newStringArray = FirstMain.insertString(stringArray, newString);
+        String[] newStringArray = FirstMain.inserisciInArray(stringArray, newString);
 
         //Stampo il mio nuovo array
         System.out.println("Nuovo array con 6 citta:");
